f33390793481fd747d664931bc0127fd6ef88fc2
Rowdy <mroddy5280@gmail.com>
2024-08-15 12:54:23 -0600

    fix(wagmi.ts): fixed error in wagmi config for setting cookies

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -10,33 +10,30 @@ export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
 if (!projectId) throw new Error("Project ID is not defined");
 
 export const wagmiConfig = createConfig(
-	getDefaultConfig({
-		ssr: true,
-		storage: createStorage({
-			storage: cookieStorage,
-		}),
-		chains: [sepolia, mainnet, optimism, arbitrum, base],
-		transports: {
-			[mainnet.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_RPC_URL}`),
-			[sepolia.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_SEPOLIA_RPC_URL}`),
-			[optimism.id]: http(
-				`${process.env.NEXT_PUBLIC_ALCHEMY_OPTIMISM_RPC_URL}`,
-			),
-			[arbitrum.id]: http(
-				`${process.env.NEXT_PUBLIC_ALCHEMY_ARBITRUM_RPC_URL}`,
-			),
-			[base.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_BASE_RPC_URL}`),
-		},
+  getDefaultConfig({
+    ssr: true,
+    chains: [sepolia, mainnet, optimism, arbitrum, base],
+    transports: {
+      [mainnet.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_RPC_URL}`),
+      [sepolia.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_SEPOLIA_RPC_URL}`),
+      [optimism.id]: http(
+        `${process.env.NEXT_PUBLIC_ALCHEMY_OPTIMISM_RPC_URL}`,
+      ),
+      [arbitrum.id]: http(
+        `${process.env.NEXT_PUBLIC_ALCHEMY_ARBITRUM_RPC_URL}`,
+      ),
+      [base.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_BASE_RPC_URL}`),
+    },
 
-		// Required API Keys
-		walletConnectProjectId: projectId,
+    // Required API Keys
+    walletConnectProjectId: projectId,
 
-		// Required App Info
-		appName: "CookieJar",
+    // Required App Info
+    appName: "CookieJar",
 
-		// Optional App Info
-		appDescription: "A Web3 slush fund for trusted groups",
-		appUrl: "https://cookiejar.wtf", // your app's url
-		// appIcon: "https://family.co/logo.png",
-	}),
+    // Optional App Info
+    appDescription: "A Web3 slush fund for trusted groups",
+    appUrl: "https://cookiejar.wtf", // your app's url
+    // appIcon: "https://family.co/logo.png",
+  }),
 );
