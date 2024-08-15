cfc81c8d0d1de0437955e62e5780b2cc85cc7d89
Rowdy <mroddy5280@gmail.com>
2024-08-14 20:36:15 -0600

    fix: fixed jar creation for hats jars

--- hooks/useCookieJarNFT.ts
+++ hooks/useCookieJarNFT.ts
@@ -3,44 +3,44 @@ import { usePublicClient } from "wagmi";
 import { ZERO_ADDRESS } from "@/app/constants";
 
 export const useCookieNFT = ({ nftAddress }: { nftAddress?: string }) => {
-	const publicClient = usePublicClient();
+  const publicClient = usePublicClient();
 
-	const { data, ...rest } = useQuery({
-		queryKey: ["nftData", { nftAddress }],
-		queryFn: () =>
-			publicClient?.getLogs({
-				address: nftAddress as `0x${string}`,
-				event: {
-					name: "Transfer",
-					inputs: [
-						{ type: "address", indexed: true, name: "from" },
-						{ type: "address", indexed: true, name: "to" },
-						{ type: "uint256", indexed: false, name: "value" },
-					],
-					type: "event",
-				},
-				args: {
-					from: ZERO_ADDRESS,
-				},
-			}),
-		enabled: !!nftAddress || !!publicClient,
-	});
+  const { data, ...rest } = useQuery({
+    queryKey: ["nftData", { nftAddress }],
+    queryFn: () =>
+      publicClient?.getLogs({
+        address: nftAddress as `0x${string}`,
+        event: {
+          name: "Transfer",
+          inputs: [
+            { type: "address", indexed: true, name: "from" },
+            { type: "address", indexed: true, name: "to" },
+            { type: "uint256", indexed: false, name: "value" },
+          ],
+          type: "event",
+        },
+        args: {
+          from: ZERO_ADDRESS,
+        },
+      }),
+    enabled: !!nftAddress || !!publicClient,
+  });
 
-	if (!nftAddress) {
-		console.log("No cookie jar address provided");
-		return undefined;
-	}
+  if (!nftAddress) {
+    console.log("No cookie jar address provided");
+    return undefined;
+  }
 
-	if (!publicClient) {
-		console.log("Not connected to network");
-		return undefined;
-	}
+  if (!publicClient) {
+    console.log("Not connected to network");
+    return undefined;
+  }
 
-	const totalSupply = data?.length;
+  const totalSupply = data?.length;
 
-	return {
-		data,
-		totalSupply,
-		...rest,
-	};
+  return {
+    data,
+    totalSupply,
+    ...rest,
+  };
 };
