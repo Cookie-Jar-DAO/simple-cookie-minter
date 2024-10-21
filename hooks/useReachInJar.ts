ecac8d3326d3aaaa877e9bb9cf1213069f87a992
Rowdy <mroddy5280@gmail.com>
2024-10-20 22:05:50 -0600

    fix(useReachInJar.ts): updated to point to the new config

--- hooks/useReachInJar.ts
+++ hooks/useReachInJar.ts
@@ -1,31 +1,31 @@
-import { useWalletClient } from "wagmi";
+import { Config, useWalletClient } from "wagmi";
 import { writeContract, simulateContract } from "wagmi/actions";
 import type { IClaimFromJarFormInput } from "@/components/ClaimFromJarForm";
-import { wagmiConfig } from "@/config/wagmi";
+import { config } from "@/config";
 import { CookieJarCore } from "../abis";
 
 export const useReachInJar = () => {
-	const walletClient = useWalletClient();
+  const walletClient = useWalletClient();
 
-	const reachInCookieJar = async (claimData: IClaimFromJarFormInput) => {
-		console.log("claimData", claimData);
-		if (!walletClient) {
-			throw new Error("Not connected to wallet");
-		}
-		console.log("claim data jar address:", claimData.cookieJarAddress);
+  const reachInCookieJar = async (claimData: IClaimFromJarFormInput) => {
+    console.log("claimData", claimData);
+    if (!walletClient) {
+      throw new Error("Not connected to wallet");
+    }
+    console.log("claim data jar address:", claimData.cookieJarAddress);
 
-		const { request } = await simulateContract(wagmiConfig, {
-			address: claimData.cookieJarAddress,
-			abi: CookieJarCore,
-			functionName: "reachInJar",
-			args: [claimData.cookieMonster, claimData.reason],
-		});
-		console.log("request", request);
+    const { request } = await simulateContract(config as Config, {
+      address: claimData.cookieJarAddress,
+      abi: CookieJarCore,
+      functionName: "reachInJar",
+      args: [claimData.cookieMonster, claimData.reason],
+    });
+    console.log("request", request);
 
-		return await writeContract(wagmiConfig, request);
-	};
+    return await writeContract(config as Config, request);
+  };
 
-	return {
-		reachInCookieJar,
-	};
+  return {
+    reachInCookieJar,
+  };
 };
