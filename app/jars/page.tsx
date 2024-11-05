945893e4db9bfc29146174013b32281562b59805
Mitja Belak <mico@0x0.icu>
2024-11-05 22:46:27 +0100

    Added icons for chains

--- app/jars/page.tsx
+++ app/jars/page.tsx
@@ -14,33 +14,43 @@ import { columns } from "./columns";
 import { cookJarMockData, data } from "./mock-data";
 import ChainJars from "@/components/chain-jars";
 
+type Chain = "Sepolia" | "Base" | "Gnosis" | "Arbitrum" | "Optimism";
+
 export interface ChainAndGraph {
   url: string;
-  chain: string;
+  name: Chain;
 }
 
 const chainGraphs: ChainAndGraph[] = [
   {
+    url: "https://api.studio.thegraph.com/query/92478/cg-sepolia/version/latest",
+    name: "Sepolia",
+  },
+  {
     url: "https://api.studio.thegraph.com/query/92478/cg-base/version/latest",
-    chain: "base",
+    name: "Base",
   },
   {
     url: "https://api.studio.thegraph.com/query/92478/cg-gnosis/version/latest",
-    chain: "gnosis",
+    name: "Gnosis",
   },
   {
     url: "https://api.studio.thegraph.com/query/92478/cg-arbitrum/version/latest",
-    chain: "arbitrum",
+    name: "Arbitrum",
   },
   {
     url: "https://api.studio.thegraph.com/query/92478/cg-optimism/version/latest",
-    chain: "optimism",
-  },
-  {
-    url: "https://api.studio.thegraph.com/query/92478/cg-sepolia/version/latest",
-    chain: "sepolia",
+    name: "Optimism",
   },
 ];
+
+const chainToImageMap: Record<Chain, string> = {
+  Sepolia: "/sepolia.webp",
+  Base: "/base.webp",
+  Gnosis: "/gnosis.webp",
+  Arbitrum: "/arbitrum.webp",
+  Optimism: "/optimism.webp",
+};
 
 export default async function JarsPage() {
   return (
@@ -54,20 +64,30 @@ export default async function JarsPage() {
           className="-z-10 bg-center object-cover blur-sm"
           fill
         />
-        <Card className="relative z-20 flex flex-col items-center gap-8 border-none bg-amber-100 p-8 text-center">
-          <h1 className="font-gluten text-5xl font-semibold">Jars</h1>
-          <ScrollArea className="h-[40rem] w-full max-w-4xl">
-            <div className="flex flex-col gap-2 p-4 pt-0">
-              {chainGraphs.map((chainAndGraph) => (
-                <ChainJars
-                  key={chainAndGraph.chain}
-                  chainAndGraph={chainAndGraph}
-                />
-              ))}
-              {/* <DataTable columns={columns} data={data} /> */}
-            </div>
-          </ScrollArea>
-        </Card>
+        {chainGraphs.map((chainAndGraph) => (
+          <Card
+            key={chainAndGraph.name}
+            className="relative z-20 flex flex-col items-center gap-8 border-none bg-amber-100 p-8 text-center"
+          >
+            <h1 className="font-gluten text-5xl font-semibold">
+              <Image
+                src={chainToImageMap[chainAndGraph.name]}
+                title={chainAndGraph.name}
+                alt="Chain logo"
+                width={80}
+                height={80}
+                className="mr-5 inline-block"
+              />
+              {chainAndGraph.name} Jars
+            </h1>
+            <ScrollArea className="h-[40rem] w-full max-w-4xl">
+              <div className="flex flex-col gap-2 p-4 pt-0">
+                <ChainJars chainAndGraph={chainAndGraph} />
+                {/* <DataTable columns={columns} data={data} /> */}
+              </div>
+            </ScrollArea>
+          </Card>
+        ))}
       </section>
     </div>
   );
