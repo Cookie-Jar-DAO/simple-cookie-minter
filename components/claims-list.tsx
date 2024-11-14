141dbcf5b41180f832cc7d4a166f29bd7f7eacfb
Michael Green <michaelgreen06@gmail.com>
2024-11-14 17:52:27 +0700

    aligned columns. Eliminated reason assessment for mvp

--- components/claims-list.tsx
+++ components/claims-list.tsx
@@ -11,15 +11,15 @@ import {
 import { AssessmentDialog } from "./assessment-dialog";
 import { type Address } from "viem";
 
-export function ClaimsList({ 
+export function ClaimsList({
   claims,
-  cookieJarAddress 
-}: { 
+  cookieJarAddress,
+}: {
   claims: Claim[];
   cookieJarAddress: Address;
 }) {
-  console.log(cookieJarAddress,"cjadd2");
-  
+  console.log(cookieJarAddress, "cjadd2");
+
   if (claims.length === 0) {
     return (
       <div className="flex h-10 items-center justify-center text-center">
@@ -30,26 +30,30 @@ export function ClaimsList({
 
   return (
     <div className="w-full space-y-2">
-      <div className="flex items-center justify-between space-x-4">
+      <div className="grid grid-cols-3 items-center justify-items-start">
         <h4 className="text-sm font-semibold">Claims</h4>
+        <h4 className="text-sm font-semibold">Reasons</h4>
+        <h4 className="text-sm font-semibold"></h4>{" "}
+        {/* Placeholder for arrow */}
       </div>
       <ScrollArea className="w-full max-w-4xl">
         <Accordion type="single" collapsible className="w-full">
           {claims.map((claim) => {
             return (
               <AccordionItem key={claim.id} value={claim.id}>
-                <AccordionTrigger>
-                  {truncateEthereumAddress(claim.receiver)}
-                  <p>{claim.reason.reason}</p>
+                <AccordionTrigger className="grid w-full grid-cols-3 items-center justify-items-start">
+                  <span>{truncateEthereumAddress(claim.receiver)}</span>
+                  <span>{claim.reason.reason}</span>
                 </AccordionTrigger>
-                <AccordionContent>
+                {/* TODO finish assessment flow. Needs to have accumulator for yes & no vote count */
+                /* <AccordionContent>
                   <div className="flex items-center justify-between">
-                    <AssessmentDialog 
-                      claimId={claim.id} 
+                    <AssessmentDialog
+                      claimId={claim.id}
                       cookieJarAddress={cookieJarAddress}
                     />
                   </div>
-                </AccordionContent>
+                </AccordionContent> */}
               </AccordionItem>
             );
           })}
